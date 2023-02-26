95990edf041e348c03c122143ea3dc0eb14c4725
robert morales <rmoralfit@gmail.com>
2023-02-26 16:49:44 -0500

    connected mongoDB to application

--- server/index.js
+++ server/index.js
@@ -5,11 +5,20 @@ import cors from 'cors';
 
 const app = express();
 
-app.arguments(bodyParser.json({ limit: "30mb", extended: true }));
-app.arguments(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.arguments(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://rMorales:rMorales123@cluster0.ismec.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = 'mongodb+srv://rMorales:rMorales123@cluster0.ismec.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+//   .catch((error) => console.log(error.message));
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(CONNECTION_URL, () => {
+    console.log("Connected to MongoDB");
+  });
+
