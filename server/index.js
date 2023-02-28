f84d3163c58600694052f1fd6e26666451a7149f
robert morales <rmoralfit@gmail.com>
2023-02-28 17:27:00 -0500

    closed node ports, fixed connection to mongoDBatlas, fixed connection to PORT

--- server/index.js
+++ server/index.js
@@ -7,20 +7,24 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes)
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://rMorales:rMorales123@cluster0.ismec.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
-// mongoose.connect(CONNECTION_URL)
-//   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-//   .catch((error) => console.log(error.message));
+app.use('/posts', postRoutes);
+
+
+const CONNECTION_URL = 'mongodb://robMorales:robMorales123@cluster0-shard-00-00.ismec.mongodb.net:27017,cluster0-shard-00-01.ismec.mongodb.net:27017,cluster0-shard-00-02.ismec.mongodb.net:27017/?ssl=true&replicaSet=atlas-45q7h9-shard-0&authSource=admin&retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
 
 mongoose.set("strictQuery", false);
-mongoose.connect(CONNECTION_URL, () => {
-    console.log("Server is running on 5000!");
-  });
+mongoose.connect(CONNECTION_URL)
+     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+     .catch((error) => console.log(`${error} did not connect`));
+
+
+// mongoose.set("strictQuery", false);
+// mongoose.connect(CONNECTION_URL, () => {
+//     console.log("Server is running!");
+//   });
 
