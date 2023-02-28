958ca4319a65c180cfbb38f5f07ceafcd1ed760e
robert morales <rmoralfit@gmail.com>
2023-02-27 19:41:05 -0500

    debugging posts

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -11,13 +15,11 @@ app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://rMorales:rMorales123@cluster0.ismec.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
-
-// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connect(CONNECTION_URL)
 //   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
 //   .catch((error) => console.log(error.message));
 
 mongoose.set("strictQuery", false);
-
 mongoose.connect(CONNECTION_URL, () => {
     console.log("Server is running on 5000!");
   });
---
+++ server/routes/posts.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('THIS WORKS!')
+});
+
+
+
+
+
+
+
+export default router;
