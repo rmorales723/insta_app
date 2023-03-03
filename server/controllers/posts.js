434fdf49630530a6b84653d5535538998af5b58e
robert morales <rmoralfit@gmail.com>
2023-03-03 18:24:31 -0500

    created getPosts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,15 @@
+import PostMessage from "../models/postMessage,js";
 
-export const getPosts = (req, res) => {
-    res.send('THIS WORKS!');
-};
+
+export const getPosts = async (req, res) => {
+  try {
+      const postMessages = await PostMessage.find();
+      res.status(200).json(postMessages);
+  } catch (error) {
+      res.status(404).json({message: error.message})
+  }
+}
+
+export const createPost = (req, res) => {
+    res.send('Post Created!');
+}
