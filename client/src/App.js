cff8b67f1d380035984c6e77da7811f3d8b27e31
robert morales <rmoralfit@gmail.com>
2023-03-04 20:45:14 -0500

    working on structure in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import memories from './images/memories.jpg';
 
 const App = () => {
     return (
-        <div>
-           <h1>HELP</h1> 
-        </div>
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="650" />
+            </AppBar>
+        </Container>
     )
 }
 
