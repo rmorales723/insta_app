dcde10bb617d767920a9ef3e348fa44eb4659cf9
robert morales <rmoralfit@gmail.com>
2023-03-05 14:19:30 -0500

    adding logic to Form styles.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import memories from './images/memories.jpg';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import useStyles from './styles';
 
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <Container maxwidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memories" height="650" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="150" />
             </AppBar>
             <Grow in>
                 <Container>
