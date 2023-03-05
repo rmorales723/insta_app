ea666740cb03dd0094f01734dba0488016d5ac3e
robert morales <rmoralfit@gmail.com>
2023-03-04 21:03:11 -0500

    working on styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import memories from './images/memories.jpg';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
 
 const App = () => {
     return (
@@ -9,6 +12,19 @@ const App = () => {
                 <Typography variant="h2" align="center">Memories</Typography>
                 <img src={memories} alt="memories" height="650" />
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+
+            </Grow>
         </Container>
     )
 }
