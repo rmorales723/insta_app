05bb50c2c5c0c17b954aa3cf2a54f84a4d0dc101
robert morales <rmoralfit@gmail.com>
2023-03-05 15:00:34 -0500

    successfully enabled redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,15 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-
 import App from './App';
+import {Provider} from 'react-redux';
+import {configureStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>,
+document.getElementById('root'));
