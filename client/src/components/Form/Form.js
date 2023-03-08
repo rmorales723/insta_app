db5cb45f5ecc5a16f06a89b3251d5b91418bc713
robert morales <rmoralfit@gmail.com>
2023-03-08 16:51:37 -0500

    added state to postData object

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
 import useStyles from './styles';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
 
 const Form = () => {
     const classes = useStyles();
+    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+
+    const handleSubmit =  () => {
+        
+      };
+
   return (
-    <h1>Form</h1>
-  )
+  <Paper className={classes.paper}>
+    <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+        <Typography variant="h6">Creating a Memory</Typography>
+            <TextField 
+            name="creator" 
+            variant="outlined" 
+            label="Creator" 
+            fullWidth
+            value={postData.creator}
+            onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
+            />
+    </form>
+  </Paper>
+  );
 }
 
 export default Form;
